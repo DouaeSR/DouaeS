cb2446cc9d93beb477749c0ace39eda2ac2278a7
azaabalkhadija <azaabalkhadija233@gmail.com>
2026-02-21 20:20:29 +0000

    add modification

--- src/main/java/com/bookstore/douaes/model/User.java
+++ src/main/java/com/bookstore/douaes/model/User.java
@@ -1,12 +1,8 @@
 package com.bookstore.douaes.model;
 
-import jakarta.persistence.*;
 import lombok.*;
-
+import jakarta.persistence.*;
 import java.util.List;
-import lombok.ToString.*;
-
-
 
 @Entity
 @Table(name = "users")
