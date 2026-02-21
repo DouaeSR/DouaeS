a0dd0e91c2f2bebd253000b1e29222c57221b44d
DouaeSR <douaesirraj10@gmail.com>
2026-02-21 20:34:40 +0000

    Fix CI/CD workflow (mvnw chmod, deterministic deploy, healthcheck retry)

--- src/test/java/com/bookstore/douaes/DouaeSApplicationTests.java
+++ src/test/java/com/bookstore/douaes/DouaeSApplicationTests.java
@@ -3,11 +3,9 @@ package com.bookstore.douaes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "JWT_SECRET=test-secret-please-change-32chars-minimum!!")
 class DouaeSApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
